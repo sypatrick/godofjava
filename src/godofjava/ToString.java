7742524876c2050260cbc577eb6e76e41b1a9347
patrick <patrick@DESKTOP-76KHOC3>
2022-08-16 21:41:30 +0900

    Object.Tostring

---
+++ src/godofjava/ToString.java
@@ -0,0 +1,19 @@
+package godofjava;
+
+public class ToString {
+
+	public static void main(String[] args) {
+		
+		ToString thisObject = new ToString();
+		
+		thisObject.toStringMethod(thisObject);
+
+	}
+	
+	public void toStringMethod(Object obj) {
+		System.out.println(obj);
+		System.out.println(obj.toString());
+		System.out.println("plus" + obj);
+	}
+
+}
