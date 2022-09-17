f7985b7c1118e087b93ecd2e25e579016a33a64d
patrick <patrick@DESKTOP-76KHOC3>
2022-09-17 19:47:01 +0900

    Singleton(Thread-safe)

---
+++ src/singleton/DoubleCheckedSingleton.java
@@ -0,0 +1,10 @@
+package singleton;
+
+public class DoubleCheckedSingleton {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/singleton/LazyInitializedSingleton.java
@@ -0,0 +1,17 @@
+package singleton;
+
+import java.util.Objects;
+
+public class LazyInitializedSingleton {
+	
+    private static LazyInitializedSingleton instance;
+    
+    private LazyInitializedSingleton() {}
+    
+    public static LazyInitializedSingleton getInstance(){
+        if(Objects.isNull(instance)) {
+            instance = new LazyInitializedSingleton();
+        }
+        return instance;
+    }
+}
